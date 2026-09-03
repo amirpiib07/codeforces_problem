2e4ecda99c3a663ee6798150d2e2dfab3ceb0a2e
AMIT KUMAR SINGH <amirpiib07@gmail.com>
2026-09-03 23:05:48 +0530

    Add Odd One Out [A] from Codeforces

---
+++ 1915/A - Odd One Out/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        int ans = a ^ b ^ c;
+        cout << ans << endl;
+    }
+    return 0;
+}
